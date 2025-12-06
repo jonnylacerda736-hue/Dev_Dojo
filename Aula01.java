2b2c9b1ac96fac0de37ca4a0ce524bcf77b0a0f2
Jonny Lacerda <jonnylacerda736@gmail.com>
2025-12-06 19:06:32 -0300

    Comentários

--- Aula01.java
+++ Aula01.java
@@ -1,4 +1,11 @@
 public class Aula01 {
+
+    //Comentário em apenas uma linha
+    /* 
+    
+    Comentários em multiplas Linhas 
+    
+    */
     public static void main(String[] args) {
         System.out.println("Como printar um valor!");
     }
