b162a112159986f40a4aac5842783caa75ad4c67
Jonny Lacerda <jonnylacerda736@gmail.com>
2025-12-06 19:55:13 -0300

    variaveis e seus valores

--- Aula02TiposPrimitivos.java
+++ Aula02TiposPrimitivos.java
@@ -2,8 +2,20 @@ public class Aula02TiposPrimitivos {
     
     public static void main(String[] args) {
         //int, double, float, char, byte, short, long, boolean
-        int idade = 10;
+        int idade = 10; 
+        long numero = 100000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'A';
+        String nome = "Jonny";
 
-        System.out.println("A idade de jorge é:" + idade);
+        System.out.println(falso);
+        System.out.println("char" + caractere);
+        System.out.println(numero);
+        System.out.println("o nome do garoto é: " +nome +" e sua idade é : "+ idade);
     }
 }
