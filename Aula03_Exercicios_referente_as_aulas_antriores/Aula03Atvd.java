6749d0eb438b0d3294a6dbafacedb7097f80ebf1
Jonny Lacerda <jonnylacerda736@gmail.com>
2025-12-06 20:21:55 -0300

    resolução da questão proposta

--- Aula03_Exercicios_referente_as_aulas_antriores/Aula03Atvd.java
+++ Aula03_Exercicios_referente_as_aulas_antriores/Aula03Atvd.java
@@ -2,4 +2,14 @@ package Aula03_Exercicios_referente_as_aulas_antriores;
 
 public class Aula03Atvd {
     
+
+    public static void main(String[] args) {
+    String name = "Jonny";
+    double endereço = 330;
+    float salario = 2500;
+    String data = "25/03/2025" ;
+
+
+        System.out.println("Eu "+ name + ",morando no endereço "+ endereço + "confirmo que recebi o sálario de "+ salario + ", na data  " +data);
+    }
 }
