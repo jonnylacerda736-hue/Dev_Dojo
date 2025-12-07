6291725ee17e7b815b42d5092607fd3cd090d7a7
Jonny Lacerda <jonnylacerda736@gmail.com>
2025-12-07 15:08:44 -0300

    Operadores aritimeticos

---
+++ Aula04Operadores.java
@@ -0,0 +1,18 @@
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        // +, -, /, *
+
+        int numero01 = 10;
+        int numero02 = 20;
+        int resultado = numero01 + numero02;
+        int resultado2 = numero01 - numero02;
+        int resultado3 = numero01 * numero02;
+        int resultado4 = numero02 / numero01;
+
+        
+        System.out.println("A soma dos valores são: "+resultado);
+        System.out.println("A subtração dos valores são: "+resultado2);
+        System.out.println("A multiplicação dos valores são: "+resultado3);
+        System.out.println("A divisão dos valores são: "+resultado4);
+    }
+}
