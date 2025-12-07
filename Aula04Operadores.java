0df0b6f28eb1168368dadca278f06bb5f798d464
Jonny Lacerda <jonnylacerda736@gmail.com>
2025-12-07 15:34:06 -0300

    Operadores de resto e lógicos

--- Aula04Operadores.java
+++ Aula04Operadores.java
@@ -9,10 +9,38 @@ public class Aula04Operadores {
         int resultado3 = numero01 * numero02;
         int resultado4 = numero02 / numero01;
 
-        
+
         System.out.println("A soma dos valores são: "+resultado);
         System.out.println("A subtração dos valores são: "+resultado2);
         System.out.println("A multiplicação dos valores são: "+resultado3);
         System.out.println("A divisão dos valores são: "+resultado4);
+
+        System.out.println("--------------------------------------------");
+
+
+        // % operador resto
+
+        int resto = 21 % 7;
+        System.out.println(resto);
+
+        // (< ) menor q))  (> ) maior q)) (<=)Menor igual))  (>=)maior igual (==) igual)) (!=)diferente))
+
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualOuMaiorQueVinte = 10 >= 20;
+        boolean isDezIgualOuMenorQueVinte = 10 <= 20;
+        boolean isDezIgualQueVinte = 10 == 20;
+        boolean isDezDiferenteQueVinte = 10 != 20;
+
+        System.out.println("10 é maior que 20: "+isDezMaiorQueVinte );
+        System.out.println("10 é menor que 20: "+isDezMenorQueVinte );
+        System.out.println("10 é maior ou igual a 20: "+isDezIgualOuMaiorQueVinte );
+        System.out.println("10 é menor ou igual a 20: "+isDezIgualOuMenorQueVinte );
+        System.out.println("10 é igual a 20: "+isDezIgualQueVinte );
+        System.out.println("10 é diferente de 20: "+isDezDiferenteQueVinte );
+
+
+
+
     }
 }
